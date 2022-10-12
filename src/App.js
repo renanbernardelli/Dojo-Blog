e848123ffdbc2547f7b8e9d7c4a1c4c2af861d93
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-12 09:09:38 +0100

    :tada: Begin a new React project - Events handle, css inline

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <Navbar></Navbar>
+
+      <div className="content">
+        <Home></Home>
+      </div>
+    </div>
+  );
+}
+
+export default App;
