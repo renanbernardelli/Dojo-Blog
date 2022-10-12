864deeac3d6c6798fd936a3746f4054e11871282
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-12 09:17:31 +0100

    :construction: UseState applied

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,28 @@
+import { useState } from "react";
+
 const Home = () => {
 
-  const handleClick = (e) => {
-    console.log('hello, ninjas', e);
+  const [userName, setUserName] = useState('Renan');
+  const [age, setAge] = useState(100);
+
+  const handleClick = () => {
+    setUserName('Carlos');
+    setAge(105);
   }
   
-  const handleClickAgain = (userName, e) => {
-    console.log(`hello ${userName}`, e.target);
-  }
+  // const handleClickAgain = (userName, e) => {
+  //   console.log(`hello ${userName}`, e.target);
+  // }
 
   return ( 
 
     <div className="home">
       <h2>Homepage</h2>
 
-    <button onClick={handleClick}>Click me</button>
-    <button onClick={(e) => handleClickAgain('Renan', e)}>Click me again</button>
+      <p>{userName} is {age} years old</p>
+
+      <button onClick={handleClick}>Click me</button>
+      {/* <button onClick={(e) => handleClickAgain('Renan', e)}>Click me again</button> */}
     </div>
   );
 }
