2a2eaa687dc98b46e00b79ff0b2ffc63a0564ffe
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-14 08:18:30 +0100

    :construction: Add a hook of an object array and map it on the page. :lipstick: Improve style.

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,22 @@ import { useState } from "react";
 
 const Home = () => {
 
-  const [userName, setUserName] = useState('Renan');
-  const [age, setAge] = useState(100);
-
-  const handleClick = () => {
-    setUserName('Carlos');
-    setAge(105);
-  }
-  
-  // const handleClickAgain = (userName, e) => {
-  //   console.log(`hello ${userName}`, e.target);
-  // }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return ( 
 
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
 
-      <p>{userName} is {age} years old</p>
-
-      <button onClick={handleClick}>Click me</button>
-      {/* <button onClick={(e) => handleClickAgain('Renan', e)}>Click me again</button> */}
+        </div>
+      ))}
     </div>
   );
 }
