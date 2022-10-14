b53a718b82a57e667ac35c73812564a28566fed5
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-14 09:11:49 +0100

    :construction: Create "Delete blog" button and use the props to handle the click event, than filter the blog list

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,6 +2,7 @@ const BlogList = (props) => { //Props can be called derectly. Ex: const BlogList
 
   const blogs = props.blogs;
   const title = props.title;
+  const handleDelete = props.handleDelete;
 
   return ( 
     
@@ -11,7 +12,7 @@ const BlogList = (props) => { //Props can be called derectly. Ex: const BlogList
         <div className="blog-preview" key={blog.id}>
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
-
+          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
         </div>
       ))}
     </div>
