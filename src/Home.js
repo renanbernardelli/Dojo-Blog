701e350a7edb4265f27a070b30c9bf4907015e13
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-14 22:05:07 +0100

    :heavy_plus_sign: Add useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,7 +13,11 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
 
     setBlogs(newBlogs);
-  }
+  };
+
+  useEffect(() => {
+    console.log('use effect ran');
+  });
 
   return ( 
 
