3e7bd1947e31d453e8f587adc4d6f0208ea72dee
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-16 15:06:11 +0100

    :white_check_mark: Creating 404 Not Found component and adding Not Found route.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -26,7 +27,11 @@ function App() {
             <Route path="/blogs/:id">
               <BlogDetails></BlogDetails>
             </Route>
-            
+
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+
           </Switch>
         </div>
       </div>
