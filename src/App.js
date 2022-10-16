dbde37e522159f36ed97871b86b4305f9578d96c
Renan Bernardelli <renanbernardelli@gmail.com>
2022-10-16 10:48:57 +0100

    :sparkles: Create "BlogDetails" component. :construction: Add "Route Parameters" for blog id.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
@@ -13,12 +14,19 @@ function App() {
 
         <div className="content">
           <Switch>
+
             <Route exact path="/">
               <Home></Home>
             </Route>
+
             <Route path="/create">
               <Create></Create>
             </Route>
+
+            <Route path="/blogs/:id">
+              <BlogDetails></BlogDetails>
+            </Route>
+            
           </Switch>
         </div>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = (props) => { //Props can be called derectly. Ex: const BlogList = ({blogs, title})
 
   const blogs = props.blogs;
@@ -7,10 +9,15 @@ const BlogList = (props) => { //Props can be called derectly. Ex: const BlogList
     
     <div className="blog-list">
       <h2>{ title }</h2>
+
       {blogs.map((blog) => (
+
         <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
       ))}
     </div>
